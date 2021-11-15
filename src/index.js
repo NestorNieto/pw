38d6387ff7d143f01d2a02025e5093f21a36495b
NNieto25 <00199019@uca.edu.sv>
2021-11-15 12:36:08 -0600

    Directorio base, instalacion de react router y router inicial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './Pages/Login/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element = {<Login/>} />
+      </Routes>
+      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
