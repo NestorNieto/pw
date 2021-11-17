ace51d5b1d9206bfe889044e7d0d6fb896158559
NestorNieto <nestor.alas2001@gmail.com>
2021-11-16 20:17:08 -0600

    Diseño de formulario de creacion/edicion de post y ruta de edicion dinamica

--- src/Routes/CreatePost/CreatePost.jsx
+++ src/Routes/CreatePost/CreatePost.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./CreatePost.module.css";
+
 const CreatePost = () => {
-    return(
-        <h1>Ruta de CreatePost</h1>
+    const titleHandler = (event) => {
+        setTitulo(event.target.value);
+    };
+    const descriptionHandler = (event) => {
+        setDescripcion(event.target.value);
+    };
+
+    const urlHandler = (event) => {
+        setUrl(event.target.value);
+    };
+
+    const { postId } = useParams();
+    const [titulo, setTitulo] = useState();
+    const [descripcion, setDescripcion] = useState();
+    const [url, setUrl] = useState(' ');
+    return (
+        <>
+            <h1>Ruta de CreatePost {postId}</h1>
+            <form className={styles.post_form}>
+                <div className= {styles.labels}>
+                    <label>
+                        Titulo
+                        <input type="text" onChange={titleHandler} name="titulo" />
+                    </label>
+                    <label>
+                        Descripcion
+                        <input type="text" onChange={descriptionHandler} name="descripcion" />
+                    </label>
+                    <label>
+                        Imagen [URL]
+                        <input type="text" onChange={urlHandler} name="url" />
+                    </label>
+                </div>
+                <div className={styles.image_container}> <img src={url} alt="post preview" onError={(event) => event.target.style.display = 'none'} /> </div>
+                <input type="submit" value="Crear post" className={styles.button}/>
+                
+            </form>
+
+
+        </>
     );
 };
 
