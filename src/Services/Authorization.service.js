60458502c6b34bb6436889ccd05da430f88e5a42
NestorNieto <nestor.alas2001@gmail.com>
2021-11-21 15:26:13 -0600

    Agregada verificacion del token en el servicio de autorizacion

--- src/Services/Authorization.service.js
+++ src/Services/Authorization.service.js
@@ -1,12 +1,18 @@
-import { SignIn } from "./Helper";
+import {
+    API_BASE_URL,
+    SignIn
+} from "./Helper";
 export const login = async (username, password) => {
     try {
-        const {token, role} = await SignIn(username, password);
-        
+        const {
+            token,
+            role
+        } = await SignIn(username, password);
+
         localStorage.setItem('token', token);
         localStorage.setItem('username', username);
         localStorage.setItem('role', role);
-        
+
         return true;
     } catch (error) {
         console.log(error);
@@ -19,3 +25,27 @@ export const logOut = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
 };
+
+export const verifyToken = async (token) => {
+    try {
+        const URL = `${API_BASE_URL}/auth/whoami`;
+        const request = {
+            "method": "GET",
+            "headers": {
+                "Authorization": `Bearer ${token}`
+            }
+        }
+        const response = await fetch (URL, request);
+        if(response.ok){
+            const {username, role} = response.json();
+            return{username: username, role: role};
+        }
+        else{
+            throw Error("El token no ha sido verificado.");
+        }
+        
+    } catch (error) {
+        console.error(error);
+        return {}
+    }
+}
